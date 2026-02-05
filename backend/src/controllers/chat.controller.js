b07eee3e38997b609e531e6da264a06d665e6d0d
Tridip Sardar <tridipsardar92@gmail.com>
2026-02-06 01:34:41 +0530

    Chat fix again

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -2,12 +2,12 @@ const { generateStreamToken } = require("../lib/stream")
 
 const getStreamToken = async (req, res) => {
     try {
-        const token = generateStreamToken(req.user.id)
-
-        res.status(200).json({ token })
+        const token = generateStreamToken(req.user._id);
+        
+        res.status(200).json({ token });
     } catch (error) {
-        console.error("Error in getStreamToken controller: ", error.message)
-        res.status(500).json({ message: "Internal server error" })
+        console.log("Error in getStreamToken controller:", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
